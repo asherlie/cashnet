0123c4ef8dda7398d08291388437f8dbf1adedf3
asherlie <lieber.asher@gmail.com>
2022-05-06 22:53:38 -0400

    nc.c is now a functional curses client for ashnet

--- nc.c
+++ nc.c
@@ -1,47 +1,221 @@
 #include <unistd.h>
+#include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ncurses.h>
+#include <sys/param.h>
+#include <stdarg.h>
+
+#include "kq.h"
+
+FILE* logfp;
+
+void writelog(const char* fmt, ...){
+    va_list args;
+    logfp = fopen("LOG", "a");
+
+    va_start(args, fmt);
+    vfprintf(logfp, fmt, args); 
+    va_end(args);
+    fclose(logfp);
+}
 
 struct window{
     WINDOW* w;
+    _Bool pad;
+    pthread_mutex_t lock;
     int lines;
+    int line_cap;
+    int xmax, ymax;
+    int scroll_pos;
 };
 
-void append_window(struct window* w, char* str){
-    mvwprintw(w->w, w->lines++, 1, str);
-    wrefresh(w->w);
+/* used for kq_reader_thread */
+struct kq_win_pair{
+    struct kq* k;
+    struct window* w;
+};
+
+void label_box(struct window* w){
+    int x, y;
+    getyx(w->w, y, x);
+    mvwprintw(w->w, 0, 4, "ASHNET WIRELESS COMMUNICATION PROTOCOL");
+    wmove(w->w, y, x);
 }
 
-void init_windows(struct window* messages, struct window* text, int text_lines){
+void window_refresh(struct window* w){
+    int start_ln;
+    if(w->pad){
+        box(w->w, 0, 0);
+        refresh();
+        start_ln = (w->lines >= w->ymax) ? w->lines-w->ymax : 0;
+        (void)start_ln;
+        label_box(w);
+        prefresh(w->w, 0, 0, 0, 0, w->lines, w->xmax);
+    }
+    else wrefresh(w->w);
+}
+
+/* this must be threadsafe because the message box is written to both when messages are being sent AND
+ * when a message is popped from the kq and displayed to the user */
+void append_window(struct window* w, char* str){
     int x, y;
-    messages->lines = text->lines = 1;
+    pthread_mutex_lock(&w->lock);
+    getyx(w->w, y, x);
+    (void)x;
+    /*wmove(w->w, y+1, 1);*/
+    wmove(w->w, y, 1);
+    wprintw(w->w, str);
+    window_refresh(w);
+    ++w->lines;
+    pthread_mutex_unlock(&w->lock);
+}
+
+void init_windows(struct window* messages, struct window* text, int input_fraction){
+    int x, y, text_lines;
+
+    messages->scroll_pos = 0;
+    messages->lines = text->lines = 0;
     messages->w = malloc(sizeof(WINDOW));
     text->w = malloc(sizeof(WINDOW));
-    messages->w = initscr();
-    getmaxyx(messages->w, y, x);
-    text->w = newwin(text_lines, x, y-text_lines, 0);
-    // how to resize relative to old dims
-    wresize(messages->w, y-text_lines, x);
-    refresh();
-    box(text->w, 0, 0);
+
+    pthread_mutex_init(&messages->lock, NULL);
+    cbreak();
+    initscr();
+    getmaxyx(stdscr, y, x);
+    text_lines = y/input_fraction;
+    text_lines = (text_lines < 3) ? 3 : text_lines;
+    // resizing later anyway
+    /*messages->w = newpad(1000, 176);*/
+    messages->w = newpad(y-text_lines, x);
     box(messages->w, 0, 0);
-    mvwprintw(messages->w, 0, 1, "MESSAGES");
-    mvwprintw(text->w, 0, 1, "HELLO");
-    wrefresh(messages->w);
+    messages->pad = 1;
+    /*getmaxyx(messages->w, y, x);*/
+    messages->line_cap = 1000;
+    messages->ymax = y-text_lines;
+    messages->xmax = x;
+
+    text->w = newwin(text_lines, x, y-text_lines, 0);
+    text->pad = 0;
+
+    scrollok(messages->w, 1);
+    idlok(messages->w, 1);
+    keypad(messages->w, 1);
+
+    // how to resize relative to old dims
+    /*wresize(messages->w, y-text_lines, x);*/
+    /*wresize(messages->w, text_lines, x);*/
+    box(text->w, '|', '+');
+    refresh();
+    /*box(messages->w, 0, 0);*/
+    /*waddstr(messages->w, "MESSAGES\n");*/
+    /*wprintw(messages->w, "MESSAGES\n");*/
+    /*mvwprintw(text->w, 0, 1, "HELLO");*/
+    window_refresh(messages);
+    /*wrefresh(messages->w);*/
     wrefresh(text->w);
 }
 
-int main(){
-    struct window msg, txt;
-    char buf[100];
-    init_windows(&msg, &txt, 6);
-    for(int i = 0; i < 20; ++i){
-        memset(buf, 0, 100);
-        sprintf(buf, "message #%i\n", i);
-        append_window(&msg, buf);
+/*
+ * writing simple readline implementation - up arrow goes back in history
+ * enter submits
+ * typing stores in a new buffer
+ * each time a key is pressed, it's written to current ting
+*/
+
+void* kq_reader_thread(void* vkw){
+    struct kq_win_pair* kw = vkw;
+    uint8_t* recvd;
+    while(1){
+        recvd = pop_kq(kw->k, NULL);
+        append_window(kw->w, (char*)recvd);
     }
-    getchar();
+}
+
+void broadcast_msg(char* msg, struct window* w, struct kq* k){
+    char buf[1000] = {0};
+    sprintf(buf, "YOU: %s", msg);
+    insert_kq(k, msg, 11);
+    append_window(w, buf);
+    writelog("append window called with %s from broadcast_msg\n", msg);
+}
+
+int main(int a, char** b){
+    struct window msg, txt;
+    struct kq k;
+    char buf[200] = {0};
+    int idx = 0, c;
+    pthread_t kq_reader_pth;
+    struct kq_win_pair kw = {.k=&k, .w=&msg};
+
+    if(a < 3)return 0;
+
+    init_windows(&msg, &txt, 7);
+    init_kq(&k, atoi(b[1]), atoi(b[2]));
+    pthread_create(&kq_reader_pth, NULL, kq_reader_thread, &kw);
+
+    /* not sure why this is necessary, but without
+     * writing whitespace to each line of the message
+     * box, the border doesn't get drawn
+     */
+    for(int i = 0; i < msg.ymax; ++i){
+        append_window(&msg, "");
+    }
+    wmove(msg.w, 1, 1);
+    wmove(txt.w, 1, 1);
+    refresh();
+    while(1){
+        /*switch((c = getchar())){*/
+        switch((c = wgetch(msg.w))){
+            case KEY_UP:
+            case KEY_LEFT:
+            case KEY_RIGHT:
+                /*for(int i = 100; i > 0; --i)*/
+                prefresh(msg.w, 100, 0, 0, 0, msg.ymax, msg.xmax);
+                break;
+            case KEY_EXIT:
+            case 'q':
+                goto cleanup;
+            case '\r':
+            case '\n':
+                buf[idx++] = '\n';
+                broadcast_msg(buf, &msg, &k);
+                /*append_window(&msg, buf);*/
+                wmove(txt.w, 1, 1);
+                memset(buf, ' ', idx);
+                /*werase(txt.w);*/
+                /*wrefresh(txt.w);*/
+/*
+ *                 hmm i'm not able to overwrite text on screen
+ *                 need to overwrite it and make room for next str
+ * 
+ *                 i'll be able to scroll through messages somehow
+ *                 and scroll through my sents as well!
+ * 
+ *                 for(int i = 0; i < idx; ++i)buf[i] = ' ';
+*/
+                /*wprintw(txt.w, 0, 1, buf);*/
+                /*waddstr(txt.w, buf);*/
+                wprintw(txt.w, buf);
+                wmove(txt.w, 1, 1);
+                memset(buf, 0, idx);
+                /*waddstr(txt.w, buf);*/
+                /*wrefresh(txt.w);*/
+                window_refresh(&txt);
+                idx = 0;
+                break;
+            case 127:
+                /*goto cleanup;*/
+            default:
+                writelog("captured a char: '%c'\n", c);
+                waddch(txt.w, c);
+                wrefresh(txt.w);
+                buf[idx++] = c;
+        }
+    }
+    /*getchar();*/
+    cleanup:
     endwin();
+    pthread_mutex_destroy(&txt.lock);
     return 0;
 }
